d4dde73f5f0a9aed9c15e481f2c2acd053c7b10a
xtt28 <155261788+xtt28@users.noreply.github.com>
2026-02-22 13:29:26 -0500

    Add iOS home screen quick actions.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { HeroUINativeProvider } from 'heroui-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { RouterAction, useQuickActionRouting } from 'expo-quick-actions/router';
+import * as QuickActions from 'expo-quick-actions';
 import 'react-native-reanimated';
 import '../global.css';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -17,12 +24,36 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems<RouterAction>([
+      {
+        id: '0',
+        title: 'Lunch',
+        icon: 'symbol:fork.knife',
+        params: {
+          href: '/lunch',
+        },
+      },
+      {
+        id: '1',
+        title: 'Buses',
+        icon: 'symbol:bus',
+        params: {
+          href: '/buses',
+        },
+      },
+    ]);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView>
         <HeroUINativeProvider>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <ThemeProvider
+            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+          >
             <Stack>
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             </Stack>
