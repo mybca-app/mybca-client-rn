eb0e93627b7e34c3cfbf52573874e20d9acbdf40
xtt28 <155261788+xtt28@users.noreply.github.com>
2026-03-05 08:47:10 -0500

    Fix layout of schedule section items.

--- components/home/section-schedule.tsx
+++ components/home/section-schedule.tsx
@@ -1,7 +1,11 @@
 import { formatTimeByLocale } from '@/helpers/datetime';
 import { components } from '@/network/openapi/v1';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Separator } from 'heroui-native';
 import { Text, View } from 'react-native';
+import { withUniwind } from 'uniwind';
+
+const StyledIonicons = withUniwind(Ionicons);
 
 export default function SectionSchedule({
   schedule,
@@ -12,13 +16,14 @@ export default function SectionSchedule({
     <View className="flex flex-col gap-2">
       <View className="flex flex-row items-center">
         <Text className="text-base text-muted grow">
-          {schedule?.name} Schedule
+          <StyledIonicons name="time" size={14} />
+          &nbsp; {schedule?.name} Schedule
         </Text>
       </View>
       {schedule?.items.map((item) => (
-        <View>
-          <View className="w-full py-2 flex flex-row gap-2">
-            <Text className="text-foreground text-base grow">
+        <View key={item.periodName + item.startTime}>
+          <View className="w-full pb-2 flex flex-row gap-2">
+            <Text className="text-foreground text-base flex-1">
               {item.periodName}
             </Text>
             <Text className="text-foreground text-base text-end">
